eee12368446139992f9884699cb9d6dc9af4b89b
noreplydev <contact.cristiansanchez@gmail.com>
2025-09-13 12:56:35 +0200

    enable tool description

--- mpcs/adan/main.py
+++ mpcs/adan/main.py
@@ -4,8 +4,8 @@ mcp = FastMCP("MyServer")
 
 @mcp.tool
 def hello(name: str) -> str:
+    """say hello"""
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    # Start an HTTP server on port 8000
     mcp.run(transport="http", host="127.0.0.1", port=8000)
